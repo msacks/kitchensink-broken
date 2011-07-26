77b1cf7a96493cade9fc90c8f0b668032e266f66
Matthew Sacks <matthew@matthewsacks.com>
2011-07-25 18:02:09 -0700

    unbreaking

--- src/com/example/MyActivity.java
+++ src/com/example/MyActivity.java
@@ -10,7 +10,8 @@ public class MyActivity extends DroidGap
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
+        super.onCreate(savedInstanceState);
         super.loadUrl("file:///android_asset/www/index.html");
-        setContentView(R.layout.main);
+       // setContentView(R.layout.main);
     }
 }
